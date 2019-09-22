9f11a6cc9f194e026e781ad207ddd59564f5a133
lukwiya23 <luk23bonnie8@gmail.com>
2019-09-22 23:41:21 +0300

    .

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ data = json.load(open("data/data.json"))
 def translate(w):
     w = w.lower()
     if w in data:
-        return w[data]
+        return data[w]
 
     else:
         return "This word is not in this dictionary"
